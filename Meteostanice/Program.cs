5ea1acc0bd02c1ae107230009da536b55e76790c
Adyxx <adam.stencl@seznam.cz>
2026-07-09 12:34:07 +0200

    Initial commit

---
+++ Meteostanice/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace Meteostanice
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
