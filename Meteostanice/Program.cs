0fbf632bd11637b34bbaf9e615f7d47c7d1a38bb
Adyxx <adam.stencl@seznam.cz>
2026-07-09 12:41:15 +0200

    download xml

--- Meteostanice/Program.cs
+++ Meteostanice/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 
-
 namespace Meteostanice
 {
     internal class Program
@@ -8,8 +7,26 @@ namespace Meteostanice
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string url = "https://pastebin.com/raw/PMQueqDV";
+
+            try
+            {
+                using HttpClient client = new HttpClient();
+
+                string xml = await client.GetStringAsync(url);
+
+                Console.WriteLine(xml);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Station is offline.");
+            
+            }
 
         }
     }
 }
+
+
+
